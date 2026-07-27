c9e398cbab597ff7663561a8097340878546b6e9
Sarah Vitoria <sarahvitoriaffranca4@gmail.com>
2026-07-27 17:58:50 -0300

    update

--- atividade2/main.py
+++ atividade2/main.py
@@ -2,9 +2,9 @@ dados = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
 
 
-print(f'a) primeiros 3:{dados[0:3]}')
+print(f'a) primeiros 3: {dados[0:3]}')
 print(f'b) Últimos 3: {dados[7:10]}')
 print(f'c) Índices pares 3: {dados[1::2]}')
 print(f'd) Índices ímpares 3: {dados[::2]}')
 print(f'e) Invertida: {dados[::-1]}')
-print(f'f) Elementos do meio 3: {dados[3:6]}')
+print(f'f) Elementos do meio 3: {dados[3:7]}')
