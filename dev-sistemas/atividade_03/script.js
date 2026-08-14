d7d0a0d7c28d3cff39789df1de12713e7ccb5cd8
Sarah-2008 <sarahvitoriaffranca4@gmail.com>
2026-08-14 14:28:15 -0300

    Exercício – Operadores Aritméticos e Lógicos

---
+++ dev-sistemas/atividade_03/script.js
@@ -0,0 +1,18 @@
+// 3. Variáveis numéricas
+let precoProduto = 20;
+let quantidade = 3;
+
+// 4. Operações
+let total = precoProduto * quantidade;
+
+console.log("Valor total da compra:", total);
+console.log("Dobro do valor:", total * 2);
+console.log("Resto da divisão do total por 2:", total % 2);
+
+// 5. Variáveis booleanas
+let cupomValido = true;
+let freteGratis = false;
+
+// 6. Operadores lógicos
+console.log("Tem algum benefício:", cupomValido || freteGratis);
+console.log("Tem todos os benefícios:", cupomValido && freteGratis);
